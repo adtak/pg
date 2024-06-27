7781c74ee4b8bfb36c678563895a84cfd1a7b407
adtak <adtak62@gmail.com>
2024-06-27 14:48:14 +0900

    add cognito user pool

--- iac/lib/iac-stack.ts
+++ iac/lib/iac-stack.ts
@@ -1,8 +1,30 @@
 import * as cdk from "aws-cdk-lib";
+import * as cognito from "aws-cdk-lib/aws-cognito";
 import { Construct } from "constructs";
 
 export class ImageDrawerStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const userPool = new cognito.UserPool(this, "UserPool", {
+      userPoolName: "image-drawer",
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      selfSignUpEnabled: false,
+    });
+    userPool.addClient("Client", {
+      userPoolClientName: "image-drawer",
+      generateSecret: false,
+      supportedIdentityProviders: [
+        cognito.UserPoolClientIdentityProvider.COGNITO,
+      ],
+      oAuth: {
+        scopes: [cognito.OAuthScope.OPENID],
+        flows: {
+          authorizationCodeGrant: true,
+        },
+      },
+    });
+    userPool.addDomain("Domain", {
+      cognitoDomain: { domainPrefix: "image-drawer" },
+    });
   }
 }
