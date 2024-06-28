85a14472c7c6b771d98409e357473aa56d8fa0da
adtak <adtak62@gmail.com>
2024-06-28 13:24:24 +0900

    apply Authenticator component

---
+++ frontend/src/amplify-config.ts
@@ -0,0 +1,14 @@
+import { ResourcesConfig } from "@aws-amplify/core";
+
+// https://docs.amplify.aws/javascript/build-a-backend/auth/use-existing-cognito-resources
+const config: ResourcesConfig = {
+  Auth: {
+    Cognito: {
+      userPoolId: "ap-northeast-1_XeF1IhZk4",
+      userPoolClientId: "15g71dfccf6u3ufikpglti93hg",
+      // identityPoolId: "<your-cognito-identity-pool-id>",
+    },
+  },
+};
+
+export default config;
