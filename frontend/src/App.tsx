a18e0a6476088b62b2c78c9bf6e449def60503fe
adtak <adtak62@gmail.com>
2024-06-28 23:16:23 +0900

    add router to all pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import "@aws-amplify/ui-react/styles.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./components/Pages/Root";
 import Home from "./components/Pages/Home";
+import Upload from "./components/Pages/Upload";
 
 import { Amplify } from "aws-amplify";
 import config from "./amplify-config";
@@ -10,12 +12,22 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home></Home>,
+      element: <Root />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
+        },
+        {
+          path: "upload",
+          element: <Upload />,
+        },
+      ],
     },
   ]);
   return (
     <>
-      <RouterProvider router={router}></RouterProvider>
+      <RouterProvider router={router} />
     </>
   );
 }
