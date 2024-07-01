fe87fb6cc527f32313b7d034b6479ae0d627a1f7
adtak <adtak62@gmail.com>
2024-07-01 19:11:03 +0900

    add download page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { Link } from "@mui/material";
 import { Amplify } from "aws-amplify";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import config from "./amplify-config";
+import Download from "./components/Pages/Download";
 import Home from "./components/Pages/Home";
 import Root from "./components/Pages/Root";
 import Upload from "./components/Pages/Upload";
@@ -45,6 +46,22 @@ function App() {
                 color="inherit"
               >
                 Upload
+              </Link>
+            ),
+          },
+        },
+        {
+          path: "download",
+          element: <Download />,
+          handle: {
+            crumb: () => (
+              <Link
+                href="/download"
+                underline="hover"
+                sx={{ display: "flex", alignItems: "center" }}
+                color="inherit"
+              >
+                Download
               </Link>
             ),
           },
