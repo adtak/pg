b122e620098992cfdd6522395691b02c5771ecbd
adtak <adtak62@gmail.com>
2024-07-03 00:06:43 +0900

    add upload button

--- frontend/src/components/Pages/Upload.tsx
+++ frontend/src/components/Pages/Upload.tsx
@@ -1,5 +1,31 @@
+import { Button, Typography } from "@mui/material";
+import { useRef } from "react";
+
 function Upload() {
-  return <h2>Upload</h2>;
+  const { inputRef, onClickInput } = useUploadRef();
+  return (
+    <>
+      <Typography variant="subtitle1" component="div" sx={{ mt: 2, ml: 1 }}>
+        Please select upload file
+      </Typography>
+      <Button variant="contained" fullWidth onClick={onClickInput}>
+        Upload File
+      </Button>
+      <input hidden type="file" ref={inputRef} onChange={uploadFile} />
+    </>
+  );
 }
+
+const useUploadRef = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClickInput = () => {
+    inputRef.current?.click();
+  };
+  return { inputRef, onClickInput };
+};
+
+const uploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+  console.log(event.target.files);
+};
 
 export default Upload;
