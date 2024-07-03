9b354f4a211ceac22a28fee9b079cd3938193a53
adtak <adtak62@gmail.com>
2024-07-03 18:37:07 +0900

    split upload button to component

---
+++ frontend/src/components/Container/UploadButton.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@mui/material";
+import { useRef } from "react";
+
+function UploadButton() {
+  const { inputRef, onClickInput } = useUploadRef();
+  return (
+    <>
+      <Button variant="contained" fullWidth onClick={onClickInput}>
+        Upload File
+      </Button>
+      <input hidden type="file" ref={inputRef} onChange={uploadFile} />
+    </>
+  );
+}
+
+const useUploadRef = () => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onClickInput = () => {
+    inputRef.current?.click();
+  };
+  return { inputRef, onClickInput };
+};
+
+const uploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
+  console.log(event.target.files);
+};
+
+export default UploadButton;
--- frontend/src/components/Pages/Upload.tsx
+++ frontend/src/components/Pages/Upload.tsx
@@ -1,31 +1,15 @@
-import { Button, Typography } from "@mui/material";
-import { useRef } from "react";
+import { Typography } from "@mui/material";
+import UploadButton from "../Container/UploadButton";
 
 function Upload() {
-  const { inputRef, onClickInput } = useUploadRef();
   return (
     <>
       <Typography variant="subtitle1" component="div" sx={{ mt: 2, ml: 1 }}>
         Please select upload file
       </Typography>
-      <Button variant="contained" fullWidth onClick={onClickInput}>
-        Upload File
-      </Button>
-      <input hidden type="file" ref={inputRef} onChange={uploadFile} />
+      <UploadButton />
     </>
   );
 }
-
-const useUploadRef = () => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const onClickInput = () => {
-    inputRef.current?.click();
-  };
-  return { inputRef, onClickInput };
-};
-
-const uploadFile = (event: React.ChangeEvent<HTMLInputElement>) => {
-  console.log(event.target.files);
-};
 
 export default Upload;
