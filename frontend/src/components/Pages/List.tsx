78efd78dc494c75ddfb6a8b442e9603a2918aad0
adtak <adtak62@gmail.com>
2024-08-28 19:50:34 +0900

    add List component

--- frontend/src/components/Pages/List.tsx
+++ frontend/src/components/Pages/List.tsx
@@ -1,3 +1,28 @@
+import { Box, Card, CardContent, Tab, Tabs, Typography } from "@mui/material";
+import type React from "react";
+import { useState } from "react";
+
 export default function List() {
-  return <h2>List</h2>;
+  const [value, setValue] = useState(0);
+  const handleChange = (_: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  return (
+    <>
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label="All" />
+        <Tab label="Dogs" />
+        <Tab label="Cats" />
+      </Tabs>
+      <Box>
+        <Card variant="outlined">
+          <CardContent>
+            <Typography variant="subtitle1" component="div">
+              Please select upload file
+            </Typography>
+          </CardContent>
+        </Card>
+      </Box>
+    </>
+  );
 }
