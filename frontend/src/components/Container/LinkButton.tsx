e2ade76f717af6f3f3a69aa9690413c9ff4c324c
adtak <adtak62@gmail.com>
2024-08-28 19:36:37 +0900

    add link button of list

--- frontend/src/components/Container/LinkButton.tsx
+++ frontend/src/components/Container/LinkButton.tsx
@@ -4,14 +4,19 @@ import { Link } from "react-router-dom";
 export default function LinkButton() {
   return (
     <Grid container spacing={2} sx={{ mb: 1 }}>
-      <Grid item xs={6}>
+      <Grid item xs={4}>
         <Button variant="contained" fullWidth component={Link} to="/upload">
           Upload
         </Button>
       </Grid>
-      <Grid item xs={6}>
+      <Grid item xs={4}>
         <Button variant="contained" fullWidth component={Link} to="/download">
           Download
+        </Button>
+      </Grid>
+      <Grid item xs={4}>
+        <Button variant="contained" fullWidth component={Link} to="/list">
+          List
         </Button>
       </Grid>
     </Grid>
