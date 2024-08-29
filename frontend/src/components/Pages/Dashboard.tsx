ffc1667350097f5ff0fed5f890436814a255817e
adtak <adtak62@gmail.com>
2024-08-29 19:11:49 +0900

    impl dashbord component useState & fetch api

--- frontend/src/components/Pages/Dashboard.tsx
+++ frontend/src/components/Pages/Dashboard.tsx
@@ -1,24 +1,47 @@
 import { Box, Card, CardContent, Tab, Tabs, Typography } from "@mui/material";
 import type React from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+type Album = {
+  id: number;
+  userId: number;
+  title: string;
+};
 
 export default function Dashboard() {
-  const [value, setValue] = useState(0);
+  const [albums, setAlbums] = useState([
+    { id: 0, userId: 0, title: "Unknown" },
+  ]);
+  const [activeAlbumId, setActiveAlbumId] = useState(0);
   const handleChange = (_: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
+    setActiveAlbumId(newValue);
   };
+  useEffect(() => {
+    const fetchAlbums = async () => {
+      const url = "https://jsonplaceholder.typicode.com/albums/?userId=1";
+      const response = await fetch(url);
+      const albums: Album[] = await response.json();
+      setAlbums(albums);
+    };
+    fetchAlbums();
+  });
   return (
     <>
-      <Tabs value={value} onChange={handleChange}>
-        <Tab label="All" />
-        <Tab label="Dogs" />
-        <Tab label="Cats" />
+      <Tabs
+        value={activeAlbumId}
+        onChange={handleChange}
+        variant="scrollable"
+        scrollButtons
+      >
+        {albums.map((album) => (
+          <Tab key={album.id} value={album.id} label={album.title} />
+        ))}
       </Tabs>
       <Box>
         <Card variant="outlined">
           <CardContent>
             <Typography variant="subtitle1" component="div">
-              Please select upload file
+              This album id is {activeAlbumId}
             </Typography>
           </CardContent>
         </Card>
