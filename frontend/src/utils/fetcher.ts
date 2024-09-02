ec5fd247e625ac114b83ae97cf77eda3a033aa51
adtak <adtak62@gmail.com>
2024-09-02 23:37:15 +0900

    move sleep function to inner

--- frontend/src/utils/fetcher.ts
+++ frontend/src/utils/fetcher.ts
@@ -1,11 +1,7 @@
 const fetcher = async (url: string) => {
-  await sleep(1000);
+  await new Promise((handler) => setTimeout(handler, 1000));
   const response = await fetch(url);
   return response.json();
-};
-
-const sleep = (ms: number) => {
-  return new Promise((handler) => setTimeout(handler, ms));
 };
 
 export { fetcher };
