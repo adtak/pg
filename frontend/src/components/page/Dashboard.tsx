cd90f021795a3bf591e5cc2308af9a8e1ad15364
adtak <adtak62@gmail.com>
2024-09-02 22:40:36 +0900

    update useAlbum to use SWR

--- frontend/src/components/page/Dashboard.tsx
+++ frontend/src/components/page/Dashboard.tsx
@@ -1,28 +1,43 @@
-import { Box, Card, CardContent, Typography } from "@mui/material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import { useAlbums } from "../../hooks/Album";
 import AlbumTabs from "../container/AlbumTabs";
 
 export default function Dashboard() {
-  const { albums, activeAlbumId, handleChange } = useAlbums();
-  return (
-    <>
-      <Typography variant="subtitle1" component="div" sx={{ mt: 2, ml: 1 }}>
-        Please select album
-      </Typography>
-      <AlbumTabs
-        albums={albums}
-        activeAlbumId={activeAlbumId}
-        handleChange={handleChange}
-      />
-      <Box>
-        <Card variant="outlined">
-          <CardContent>
-            <Typography variant="subtitle1" component="div">
-              This album id is {activeAlbumId}
-            </Typography>
-          </CardContent>
-        </Card>
+  const { data, isLoading, activeAlbumId, handleChange } = useAlbums();
+  if (isLoading) {
+    return (
+      <Box sx={{ marginTop: "5rem", textAlign: "center" }}>
+        <CircularProgress />
       </Box>
-    </>
-  );
+    );
+  }
+  if (data) {
+    return (
+      <>
+        <Typography variant="subtitle1" component="div" sx={{ mt: 2, ml: 1 }}>
+          Please select album
+        </Typography>
+        <AlbumTabs
+          albums={data}
+          activeAlbumId={activeAlbumId}
+          handleChange={handleChange}
+        />
+        <Box>
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="subtitle1" component="div">
+                This album id is {activeAlbumId}
+              </Typography>
+            </CardContent>
+          </Card>
+        </Box>
+      </>
+    );
+  }
 }
