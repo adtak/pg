57c8ef846f9b9f194c9779e2e7d1bbc23d66c6cb
adtak <adtak62@gmail.com>
2024-09-03 00:24:44 +0900

    add usePhoto

---
+++ frontend/src/hooks/Photo.ts
@@ -0,0 +1,13 @@
+import useSWR from "swr";
+import type { Photo } from "../models/Photo";
+import { fetcher } from "../utils/fetcher";
+
+const usePhoto = (albumId: number | undefined) => {
+  const url = albumId
+    ? `https://jsonplaceholder.typicode.com/albums/${albumId}/photos`
+    : null;
+  const { data, isLoading } = useSWR<Photo[]>(url, fetcher, {});
+  return { data, isLoading };
+};
+
+export { usePhoto };
