edaf5a4ad9d5facb7ec0c7ffc2dbe8890e891704
adtak <adtak62@gmail.com>
2024-10-16 22:32:42 +0900

    add get album endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
+from app.models import Album
+
 app = FastAPI()
 
 
 @app.get("/")
 async def root() -> dict:
     return {"message": "Hello World."}
+
+
+@app.get("/albums")
+async def get_albums(album_id: int | None = None) -> list[Album]:
+    return [Album(id=album_id, name="First Album")]
