35ea1f6863a33ed71e00ae1d0c4a20cccef31b32
adtak <adtak62@gmail.com>
2024-10-17 09:08:47 +0900

    add photo endpoint

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,9 +3,9 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI
 
-from app.db import Album as AlbumTable
+from app.db import Photo as PhotoTable
 from app.deps import db_conn
-from app.models import Album, AlbumAttr
+from app.models import Photo, PhotoAttr
 
 app = FastAPI()
 
@@ -15,21 +15,21 @@ async def root() -> dict:
     return {"message": "Hello World."}
 
 
-@app.get("/albums")
-async def get_albums(
+@app.get("/photos")
+async def get_photos(
     db_conn: Annotated[sqlite3.Connection, Depends(db_conn)],
-    album_id: int | None = None,
-) -> list[Album]:
-    album_table = AlbumTable(db_conn)
-    res = album_table.read(_id=album_id)
-    return [Album(id=r["id"], name=r["name"], desc=r["desc"]) for r in res]
+    photo_id: int | None = None,
+) -> list[Photo]:
+    photo_table = PhotoTable(db_conn)
+    res = photo_table.read(_id=photo_id)
+    return [Photo(id=r["id"], url=r["url"], comment=r["comment"]) for r in res]
 
 
-@app.post("/albums")
-async def post_albums(
+@app.post("/photos")
+async def post_photos(
     db_conn: Annotated[sqlite3.Connection, Depends(db_conn)],
-    album: AlbumAttr,
-) -> Album:
-    album_table = AlbumTable(db_conn)
-    res = album_table.create(album.name, album.desc)
-    return Album(id=res["id"], name=res["name"], desc=res["desc"])
+    photo: PhotoAttr,
+) -> Photo:
+    photo_table = PhotoTable(db_conn)
+    res = photo_table.create(photo.name, photo.desc)
+    return Photo(id=res["id"], url=res["url"], comment=res["comment"])
