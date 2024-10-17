64233a8532e7f35e3122b018733d563fc8a00a52
adtak <adtak62@gmail.com>
2024-10-17 09:01:50 +0900

    modify to return db conn from deps

--- backend/app/deps.py
+++ backend/app/deps.py
@@ -1,11 +1,12 @@
+import sqlite3
 from collections.abc import AsyncGenerator
 
-from app.db import Album, get_db_conn
+from app.db import get_db_conn
 
 
-async def album() -> AsyncGenerator[Album, None]:
-    db = Album(get_db_conn())
+async def db_conn() -> AsyncGenerator[sqlite3.Connection, None]:
+    conn = get_db_conn()
     try:
-        yield db
+        yield conn
     finally:
-        db.conn.close()
+        conn.close()
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,10 @@
+import sqlite3
 from typing import Annotated
 
 from fastapi import Depends, FastAPI
 
 from app.db import Album as AlbumTable
-from app.deps import album
+from app.deps import db_conn
 from app.models import Album, AlbumAttr
 
 app = FastAPI()
@@ -16,17 +17,19 @@ async def root() -> dict:
 
 @app.get("/albums")
 async def get_albums(
-    album_table: Annotated[AlbumTable, Depends(album)],
+    db_conn: Annotated[sqlite3.Connection, Depends(db_conn)],
     album_id: int | None = None,
 ) -> list[Album]:
+    album_table = AlbumTable(db_conn)
     res = album_table.read(_id=album_id)
     return [Album(id=r["id"], name=r["name"], desc=r["desc"]) for r in res]
 
 
 @app.post("/albums")
 async def post_albums(
-    album_table: Annotated[AlbumTable, Depends(album)],
+    db_conn: Annotated[sqlite3.Connection, Depends(db_conn)],
     album: AlbumAttr,
 ) -> Album:
+    album_table = AlbumTable(db_conn)
     res = album_table.create(album.name, album.desc)
     return Album(id=res["id"], name=res["name"], desc=res["desc"])
