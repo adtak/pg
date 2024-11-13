47ee00e4fd0d4616d21a0013bf129da6f74caada
adtak <adtak62@gmail.com>
2024-11-13 18:08:22 +0900

    update deps

--- backend/app/deps.py
+++ backend/app/deps.py
@@ -1,12 +1,20 @@
-import sqlite3
-from collections.abc import AsyncGenerator
-
-from app.db import get_db_conn
+from app.db.core import Session
+from app.db.tables import Album, Photo
 
 
-async def db_conn() -> AsyncGenerator[sqlite3.Connection, None]:
-    conn = get_db_conn()
-    try:
-        yield conn
-    finally:
-        conn.close()
+class CreateAlbum:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def run(self, name: str, desc: str) -> Album:
+        with self.session.begin() as session:
+            return Album.create(session, name, desc)
+
+
+class CreatePhoto:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def run(self, url: str, comment: str, album_id: int) -> Album:
+        with self.session.begin() as session:
+            return Photo.create(session, url, comment, album_id)
