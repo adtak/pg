027c5a1a00bb89555167b45772c13650a8d557fd
adtak <adtak62@gmail.com>
2024-11-13 18:10:12 +0900

    add Read deps

--- backend/app/deps.py
+++ backend/app/deps.py
@@ -11,10 +11,28 @@ class CreateAlbum:
             return Album.create(session, name, desc)
 
 
+class ReadAlbum:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def run(self, _id: int) -> Album:
+        with self.session.begin() as session:
+            return Album.read(session, _id)
+
+
 class CreatePhoto:
     def __init__(self, session: Session) -> None:
         self.session = session
 
-    def run(self, url: str, comment: str, album_id: int) -> Album:
+    def run(self, url: str, comment: str, album_id: int) -> Photo:
         with self.session.begin() as session:
             return Photo.create(session, url, comment, album_id)
+
+
+class ReadPhoto:
+    def __init__(self, session: Session) -> None:
+        self.session = session
+
+    def run(self, _id: int) -> Photo:
+        with self.session.begin() as session:
+            return Photo.read(session, _id)
