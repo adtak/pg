328dd93514dfc77547b65b515f811ddd97a09ea8
adtak <adtak62@gmail.com>
2024-11-13 18:08:12 +0900

    add session from session maker

--- backend/app/db/core.py
+++ backend/app/db/core.py
@@ -1,3 +1,16 @@
+from collections.abc import Generator
+from typing import Annotated
+
+from fastapi import Depends
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 engine = create_engine("sqlite:///database.db", echo=True, pool_pre_ping=True)
+session_maker = sessionmaker(engine)
+
+
+def get_session() -> Generator[sessionmaker, None, None]:
+    yield session_maker
+
+
+Session = Annotated[sessionmaker, Depends(get_session)]
