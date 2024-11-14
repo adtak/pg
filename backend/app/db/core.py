0b288f7c1970bebce7e9978fc3e97bea71105502
adtak <adtak62@gmail.com>
2024-11-14 13:33:19 +0900

    create db url from env

--- backend/app/db/core.py
+++ backend/app/db/core.py
@@ -1,13 +1,25 @@
+import os
 from collections.abc import Generator
 from typing import Annotated
 
+import dotenv
 from fastapi import Depends
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from app.db.tables import Base
 
-engine = create_engine("sqlite:///database.db", echo=True, pool_pre_ping=True)
+
+def get_url() -> str:
+    dotenv.load_dotenv()
+    username = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    database = os.getenv("DB_NAME")
+    return f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(get_url(), echo=True, pool_pre_ping=True)
 session_maker = sessionmaker(engine)
 
 
